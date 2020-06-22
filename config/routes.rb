504b25f255e9da583199b7c8762760f2e88c97a8
naokiweblog <nk1110.chiba@gmail.com>
2020-06-22 16:04:25 +0900

    devise-controller-model-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :students, controllers: {
+    sessions:      'students/sessions',
+    passwords:     'students/passwords',
+    registrations: 'students/registrations'
+  }
+  devise_for :teachers, controllers: {
+    sessions:      'teachers/sessions',
+    passwords:     'teachers/passwords',
+    registrations: 'teachers/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
