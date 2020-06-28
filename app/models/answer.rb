58c36da618bcf63c2c484aae47419e2a53f67919
naokiweblog <nk1110.chiba@gmail.com>
2020-06-28 17:56:41 +0900

    change-model-answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :room
-  belongs_to :teacher
   belongs_to :student
 
   validates :question, presence: true
   validates :content, presence: true
-  validates :score, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,7 +4,6 @@ class Teacher < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :answers
   has_many :room_teachers
   has_many :rooms, through: :room_teachers
 
