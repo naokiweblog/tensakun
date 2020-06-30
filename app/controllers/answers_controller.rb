f389257392a232c76445e3d384d4943110911f9b
naokiweblog <nk1110.chiba@gmail.com>
2020-06-30 18:29:57 +0900

    create-anser-index

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,16 +2,16 @@ class AnswersController < ApplicationController
   before_action :set_room
 
   def index
-    @answers = Answer.all
+    @answer = Answer.new
+    @answers = @room.answers.includes(:student)
   end
   def create
-    @answer = @room.answers.new(answer_params)
+    @answer = @room.answers.create(answer_params)
   end
 
   private
   def answer_params
-    
-    params.require(:answer).permit(:question, :content, :hint, :score).merge(student_id: current_student.id)
+    params.require(:answer).permit(:question, :content, :hint, :score).merge(student_id: current_student.id, room_id: params[:room_id])
   end
   def set_room
     @room = Room.find(params[:room_id])
