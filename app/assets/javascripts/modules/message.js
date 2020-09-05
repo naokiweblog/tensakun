506fdc1a41f972d5f7871bef4931b712bd013203
naokiweblog <nk1110.chiba@gmail.com>
2020-09-05 22:52:36 +0900

    create-message

---
+++ app/assets/javascripts/modules/message.js
@@ -0,0 +1,55 @@
+$(function(){
+  function buildMessage(message){
+    if (message.image) {
+      let html = `<div class="One-message" data-message-id=${message.id}>
+                    <div class="One-message__content">
+                      <div class="One-message__content--value">
+                        ${message.content}
+                      </div>
+                    </div>
+                    <image class="One-message__image" src="${message.image}">
+                    <div class="One-message__time">
+                      ${message.created_at}
+                    </div>
+                  </div>`
+      return html
+    } else {
+      let html = `<div class="One-message" data-message-id=${message.id}>
+                    <div class="One-message__content">
+                      <div class="One-message__content--value">
+                        ${message.content}
+                      </div>
+                    </div>
+                    <div class="One-message__time">
+                      ${message.created_at}
+                    </div>
+                  </div>`
+      return html
+    }
+  }
+
+  $(".MessagePost").on("submit", function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildMessage(data);
+      $(".Messages").append(html);
+      $(".MessagePost__submit").prop("disabled", false);
+      $(".MessagePost")[0].reset();
+      $(".Messages").animate({ scrollTop: $(".Messages")[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $(".MessagePost__submit").prop("disabled", false);
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.content @message.content
+json.image @message.image.url
